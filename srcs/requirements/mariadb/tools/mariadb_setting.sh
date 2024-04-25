7ecf9c75f89ba87c8ac49888a3f73105aac3e304
bleuuone <bleuuone@gmail.com>
2024-04-25 04:59:09 +0100

    Fix:Error Establishing Database Connection

--- srcs/requirements/mariadb/tools/mariadb_setting.sh
+++ srcs/requirements/mariadb/tools/mariadb_setting.sh
@@ -3,8 +3,9 @@
 # Start MariaDB service
 service mariadb start
 sleep 5
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mariadb.conf.d/50-server.cnf
 
-# Secure MariaDB installation
+Secure MariaDB installation
 mysql_secure_installation << EOF > /dev/null 2>&1
 n
 $MYSQL_ROOT_PASSWORD
@@ -20,7 +21,7 @@ EOF
 cat <<EOF > db1.sql
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE_NAME;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
-GRANT ALL PRIVILEGES ON '$MYSQL_DATABASE_NAME'.* TO '$MYSQL_USER'@'%';
+GRANT ALL PRIVILEGES ON $MYSQL_DATABASE_NAME.* TO '$MYSQL_USER'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 FLUSH PRIVILEGES;
 EOF
@@ -31,6 +32,7 @@ rm db1.sql
 
 # Shutdown MariaDB service
 mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
+
 
 # Start MariaDB service
 mariadbd
