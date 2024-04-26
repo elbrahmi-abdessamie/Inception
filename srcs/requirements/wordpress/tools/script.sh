440d40eaee576d08dcfd05aa3f6b58a3e549de3e
bleuuone <bleuuone@gmail.com>
2024-04-26 07:10:18 +0100

    Add redis for cache

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -28,4 +28,6 @@ wp core download --path=/var/www/html/ --allow-root
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --skip-email --allow-root
 wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www/html --allow-root
 service php7.4-fpm stop
+
+# chmod -R 555 /var/www/html
 /usr/sbin/php-fpm7.4 -F
