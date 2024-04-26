db41c61f2e7d036035c895f7b8d62c460979bfe4
bleuuone <bleuuone@gmail.com>
2024-04-26 21:35:00 +0100

    Redis ready to work

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -22,11 +22,14 @@ sed -i "s|password_here|${MYSQL_PASSWORD}|g" "$file"
 
 wp core download --path=/var/www/html/ --allow-root
 
-# wp plugin update --all --path=/var/www/html --allow-root
 
 
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --skip-email --allow-root
 wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www/html --allow-root
+wp plugin install redis-cache --activate --path=/var/www/html --allow-root
+wp plugin update --all --path=/var/www/html --allow-root
+wp redis enable --path=/var/www/html  --allow-root
+
 service php7.4-fpm stop
 
 # chmod -R 555 /var/www/html
