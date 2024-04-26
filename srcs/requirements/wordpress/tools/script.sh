06bb9a64062c70e0d2bd16bffed6256e2b2df74d
elbrahmi-abdessamie <elbrahmi-abdessamie@users.noreply.github.com>
2024-04-26 03:35:04 +0100

    Fix Redirection wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -25,7 +25,6 @@ wp core download --path=/var/www/html/ --allow-root
 # wp plugin update --all --path=/var/www/html --allow-root
 
 
-# wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --allow-root
 wp core install --url=$WP_URL --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --path=/var/www/html --skip-email --allow-root
 wp user create ${WP_USER} ${WP_EMAIL} --role=author --user_pass=${WP_PASSWORD} --path=/var/www/html --allow-root
 service php7.4-fpm stop
